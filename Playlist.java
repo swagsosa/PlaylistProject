c36662da16078f26580c20989004ad4903baef1f
Cole Mallinger <cmallinger24@students.polytechnic.org>
2023-01-19 19:41:10 -0800

    work

--- Playlist.java
+++ Playlist.java
@@ -1,80 +1,78 @@
 
-
-import java.util.ArrayList;
-
-/**
- * The Playlist class, which will keep track of a playlist of Song objects
- * Refer to the project description to make sure you have access to all available methods
- */
-public class Playlist {
-    /**
-     * Fields-- This will likely just need to be the ArrayList of Songs. Reference our previous problems
-     * (CarDealership, Zoo) for structure on how this will look
-     */
-    public ArrayList<Song> songs;
-    private String title, artist, duration;
-    private boolean status;
+import java.util.*;
+public class Playlist{
+public ArrayList<Song> playlist = new ArrayList<Song>();
 
 
-     /**
-      * Constructor-- this doesn't need any parameters. You should just initialize the ArrayList and
-      * then use additional methods to add Songs in one-by-one
-      */
-
-    public Playlist(){
-        this.title = title; this.artist = artist; this.duration = duration; this.status = status;
-        songs = new ArrayList<Song>();
+public void getPlaylist(){
+for(Song s : playlist){
+    if(s.isLiked()){
+        System.out.print("❤️ ");
     }
-    
+    else{System.out.print("- ");}
+System.out.println("\"" + s.getName() + "\" by " + s.getArtist() + " (" + s.getDuration() + ")");
+}
+}
 
-      /**
-       * Methods-- Remember that you need to be able to complete all of the following:
-       * Adding a song
-       * 'liking' a song
-       * Removing a specific song
-       * Examining all Songs (a String return or void print makes sense here)
-       * Examining a sublist of all liked songs
-       * Determining the total duration of all songs
-       * Removing all unliked songs from the playlist (careful with this one!)
-       */
 
-    public void getSongs(){
-        for(Song x : songs){
-            if(x.is)
+public void getLikedSongs(){
+    for(Song s : playlist){
+        if(s.isLiked()){
+            System.out.println("❤️ \"" + s.getName() + "\" by " + s.getArtist() + " (" + s.getDuration() + ")");
         }
-    }
-    
-    public void addPlaylist(Song s){
-        songs.add(s);
-    }
-
-    public void likeSong(int pos){
-        if(pos >= 0 && pos < songs.size()){
-            Song s = songs.get(pos);
-            s.like();
-        } else{
-            System.out.println("No song with that index exists!");
-        }
-    }
-
-    public void remove(int pos){
-        songs.remove(pos);
-    }
-
-    public void totalduration(Song s){
-       int total = 0;
-        for (int i = 0; i < songs.size() - 1; i++){
-            
-            s.getDuration();
     }
 }
 
-    public void removeLiked(){
-        for(int i = 0; i < songs.size(); i++){
-            if(songs.get(i).isLiked()){
-                songs.remove(i);
-                i--;
-            }
+
+public void addSong(Song song){
+    playlist.add(song);
+
+}
+
+
+public void removeSong(int i){
+    playlist.remove(i);
+
+}
+
+
+public void likeSong(int i){
+    if(i>=0 && i<playlist.size()){
+        Song song = playlist.get(i);
+        song.like();
+    } else{
+        System.out.println("No song with that index!");
+    }
+}
+
+public String getTotal(){
+    int count = 0;
+    for(Song s : playlist){
+        String wordNum = s.getDuration();
+        int min = Integer.parseInt(wordNum.substring(0,1));
+        int sec = Integer.parseInt(wordNum.substring(2));
+        count = count + sec + min*60; 
+    }
+
+    String finalMin = Integer.toString(count/60);
+    String finalSec;
+    int sec = count % 60;
+    if(sec < 10){
+        finalSec = "0" + Integer.toString(sec);
+    }else {
+        finalSec = Integer.toString(sec);
+    }
+
+    String finalTime = finalMin + ":" + finalSec;
+    return finalTime;
+}
+
+
+public void removeLiked(){
+    for(int i = 0; i < playlist.size() ; i++){
+        if(playlist.get(i).isLiked()){
+            playlist.remove(i);i--;
         }
     }
+}
 }
